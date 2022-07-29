4f9cf261386ccbc9793f4037ef363c7e9570f5ed
DennisDv24 <jokarboxing@gmail.com>
2022-07-29 21:33:44 +0200

    prototype: styling

---
+++ src/components/Contents.tsx
@@ -0,0 +1,28 @@
+
+import { FC } from 'react';
+
+export const Contents: FC = () => {
+	return (
+		<div className='Content'>
+			<div className='MainHeading'>
+				<h1>
+					MyIndex Maker
+				</h1>
+				<h2>
+					Milady and Remilia derived collections
+				</h2>
+			</div>
+			<nav className='MainNav'>
+				<button>Index</button>
+				<button>Audit projects</button>
+				<button>About</button>
+			</nav>
+			<p>
+			Consectetur molestias corporis optio possimus voluptate
+			reiciendis Illum recusandae sequi quisquam quo nostrum? Maiores
+			fuga inventore nesciunt amet dolor dicta quo! Nihil soluta error
+			quaerat doloremque itaque omnis magnam Quo.
+			</p>
+		</div>
+	)
+}
