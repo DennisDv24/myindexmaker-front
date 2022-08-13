3a26ee73ae2ef4da318571798f961ea85c4d27b5
Gabriel Agustin Uriarte <gabri02.uri@gmail.com>
2022-08-13 17:15:37 +0200

    feat: added DerivedCollection model

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,9 +1,32 @@
+export class DerivedCollection implements Collection {
+    
+    readonly name: string;
+    readonly img: string;
+    readonly volume: number;
+    readonly supply: number;
+    readonly tokensPerWallet: string;
+    readonly daoRank: number;
+    readonly address: string;
+
+    constructor ({ name, img, volume, supply, tokensPerWallet, daoRank, address } : Collection) {
+        this.name = name;
+        this.img = img;
+        this.volume = volume;
+        this.supply = supply;
+        this.tokensPerWallet = tokensPerWallet;
+        this.daoRank = daoRank;
+        this.address = address;
+    }
+
+
+}
+
 export interface Collection {
     name: string;
     img: string;
     volume: number;
     supply: number;
-    tokensPerWallet: number;
+    tokensPerWallet: string;
     daoRank: number;
-    address: number;
+    address: string;
 }
