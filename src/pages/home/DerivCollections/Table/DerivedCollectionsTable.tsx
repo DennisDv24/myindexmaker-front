1723990fbdf40f3c427a5649b0b3bbdbb2b18665
Gabriel Agustin Uriarte <gabri02.uri@gmail.com>
2022-08-13 21:43:03 +0200

    feat: update config element with new fields

--- src/pages/home/DerivCollections/Table/DerivedCollectionsTable.tsx
+++ src/pages/home/DerivCollections/Table/DerivedCollectionsTable.tsx
@@ -9,13 +9,45 @@ import './derivedTable.css'
 import { constants } from '../../../../contracts/contracts'
 import CollectionService from '../../../../services/CollectionService/CollectionService'
 
+const headerClassName = 'derived-table__heading';
+const defaultCellClassName = 'derived-table__cell';
+const roundedCellClassName = `${defaultCellClassName} derived-table__cell--item derived-table__cell--bordered`;
+
 const configElements: ElementTable<Collection>[] = [
-    { label: '', accessor: 'img', sortable: false, cellClassName: '' },
-    { label: 'Name', accessor: 'name', sortable: true, cellClassName: '' },
-    { label: 'Volume', accessor: 'volume', sortable: true, cellClassName: '' },
-    { label: 'Supply', accessor: 'supply', sortable: true, cellClassName: '' },
-    { label: 'Avg wallet', accessor: 'tokensPerWallet', sortable: true, cellClassName: '' },
-    { label: 'Rank', accessor: 'daoRank', sortable: true, cellClassName: '' }
+    {
+        label: '', accessor: 'img',
+        sortable: false, headerClassName: headerClassName,
+        cellClassName: `${defaultCellClassName} derived-table__cell--item-image derived-table__cell--bordered`,
+        imageProps: {
+            className: 'derived-table__image',
+        }
+    },
+    {
+        label: 'Name', accessor: 'name',
+        sortable: true, headerClassName: headerClassName,
+        cellClassName: roundedCellClassName
+    },
+    {
+        label: 'Volume', accessor: 'volume',
+        parserValue: (value) => `${value} Ξ`, 
+        sortable: true, headerClassName: headerClassName,
+        cellClassName: roundedCellClassName
+    },
+    {
+        label: 'Supply', accessor: 'supply',
+        sortable: true, headerClassName: headerClassName,
+        cellClassName: roundedCellClassName
+    },
+    {
+        label: 'Avg wallet', accessor: 'tokensPerWallet',
+        sortable: true, headerClassName: headerClassName,
+        cellClassName: roundedCellClassName
+    },
+    {
+        label: 'Rank', accessor: 'daoRank',
+        sortable: true, headerClassName: headerClassName,
+        cellClassName: roundedCellClassName
+    }
 ]
 
 export const DerivedCollectionsTable: FC = () => {
@@ -26,6 +58,9 @@ export const DerivedCollectionsTable: FC = () => {
         <Table
             caption='Milady and Remilia derived collections'
             items={[]}
+            tableStyles={{
+                tableClass: 'derived-table',
+            }}
             loadMoreOptions={{
                 initialVisibleItems: 1,
                 stepsVisibleItems: 1,
